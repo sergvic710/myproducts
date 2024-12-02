855c7a6da4f18e9494f0d379504af88d8f00a741
sergei <sergvic71@gmail.com>
2024-12-02 21:41:55 +0200

    iniyt

---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Laravel</title>
+
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+
+    <!-- Styles / Scripts -->
+    @vite(['resources/css/style.css', 'resources/css/satoshi.css','resources/js/index.js'])
+</head>
+<body
+    x-data="{ page: 'ecommerce', 'loaded': true, 'darkMode': true, 'stickyMenu': false, 'sidebarToggle': false, 'scrollTop': false }"
+    x-init="
+         darkMode = JSON.parse(localStorage.getItem('darkMode'));
+         $watch('darkMode', value => localStorage.setItem('darkMode', JSON.stringify(value)))"
+    :class="{'dark text-bodydark bg-boxdark-2': darkMode === true}"
+>
+{{--@include('partials.preloader')--}}
+<div class="flex h-screen overflow-hidden">
+</div>
+
+</body>
+</html>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,4 @@
+@extends('layouts.main')
+@section('content')
+    <h1 class="text-2xl text-gray-500">Welcome to the app</h1>
+@endsection
