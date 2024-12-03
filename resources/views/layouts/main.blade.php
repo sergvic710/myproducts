93a2bb6759a5326993664ad182bb9d9d0bb775e2
sergei <sergvic71@gmail.com>
2024-12-03 21:12:22 +0200

    Get flowbite admin panel

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -11,18 +11,13 @@
     <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
     <!-- Styles / Scripts -->
-    @vite(['resources/css/style.css', 'resources/css/satoshi.css','resources/js/index.js'])
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
-<body
-    x-data="{ page: 'ecommerce', 'loaded': true, 'darkMode': true, 'stickyMenu': false, 'sidebarToggle': false, 'scrollTop': false }"
-    x-init="
-         darkMode = JSON.parse(localStorage.getItem('darkMode'));
-         $watch('darkMode', value => localStorage.setItem('darkMode', JSON.stringify(value)))"
-    :class="{'dark text-bodydark bg-boxdark-2': darkMode === true}"
->
-{{--@include('partials.preloader')--}}
-<div class="flex h-screen overflow-hidden">
+<body>
+@include('partipals.navbar')
+@include ('partipals.aside')
+<div class="p-4 sm:ml-64">
+@yield('content')
 </div>
-
 </body>
 </html>
