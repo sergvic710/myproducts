d1bd39d0346acd5d778c0d2f1fed427679bf5832
sergei <sergvic71@gmail.com>
2025-01-07 19:02:22 +0200

    modify field

--- database/migrations/2025_01_06_194623_add_field_product_link__shop_products_table.php
+++ database/migrations/2025_01_06_194623_add_field_product_link__shop_products_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('products', function (Blueprint $table) {
-            $table->string('product_link_shop', 64)->noullable();
+            $table->string('product_link_shop', 64)->default('');
         });
     }
 
